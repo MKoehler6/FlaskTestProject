5d5f528cdb0837d9ff8a3f40c03fcf0e54cc6f99
Michael Köhler <michakoehler@posteo.de>
2021-06-25 08:33:27 +0200

    debug prints added

--- RestPlusAPI/api/shop/endpoints/products.py
+++ RestPlusAPI/api/shop/endpoints/products.py
@@ -9,6 +9,7 @@ from RestPlusAPI.api.shop.domain_logic import create_product
 from RestPlusAPI.database import db as database
 
 namespace = api.namespace('shop/products', description='Operations on my shop items')
+productName = ''
 
 
 @namespace.route('/')    # = /api/shop/products/
@@ -20,11 +21,13 @@ class Offer(Resource):
         args = pagination.parse_args(request)
         page = args.get('page', 1)
         items_per_page = args.get('items_per_page', 10)
+        print(page , "  " , items_per_page)
         products = Product.query.paginate(page, items_per_page, error_out=False)
         return products
 
     @api.expect(product)
     def post(self):
+        print(request.json)
         create_product(request.json)
         return None, 200
 
